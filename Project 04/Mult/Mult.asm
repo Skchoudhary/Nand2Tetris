72ed132f4e891f64ee0b943a37c238f619b7ee8d
Sandeep Choudhary <sandeepchoudhary1507@gmail.com>
2025-01-20 16:52:07 +0530

    Add missing file

--- Project 04/Mult/Mult.asm
+++ Project 04/Mult/Mult.asm
@@ -1,10 +1,25 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/4/Mult.asm
+@R2
+M=0
+@R1
+D=M
+@x
+M=D
 
-// Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-// The algorithm is based on repetitive addition.
+(Loop)
+@x
+D=M
+@END
+D;JLE
 
-//// Replace this comment with your code.
+@R0
+D=M
+@R2
+M=D+M
+@R1
+M=M-1
+@Loop
+0;JMP
+
+(END)
+@END
+0;JMP
